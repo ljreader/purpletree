8ec893ee6b258533e68b129efffe16d25de9c05c
ljreader <lawrencereader@outlook.com>
2017-11-09 18:43:55 -0700

    Foxtrot

--- src/facebook.js
+++ src/facebook.js
@@ -20,27 +20,14 @@ $(document).ready(function (){
        }
        
 
-        window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '1994680544133095',
+       window.fbAsyncInit = function() {
+       FB.init({
+         appId      : '1994680544133095',
          cookie     : true,  // enable cookies to allow the server to access
                              // the session
          xfbml      : true,  // parse social plugins on this page
          version    : 'v2.8' // use graph api version 2.8
-    });
-      
-    FB.AppEvents.logPageView();   
-      
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
+       });
 
        // Now that we've initialized the JavaScript SDK, we call
        // FB.getLoginStatus().  This function gets the state of the
@@ -58,8 +45,17 @@ $(document).ready(function (){
          statusChangeCallback(response);
        });
 
-              // Load the SDK asynchronously
-       
+       };
+
+       // Load the SDK asynchronously
+       (function(d, s, id) {
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) return;
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+
        // Here we run a very simple test of the Graph API after login is
        // successful.  See statusChangeCallback() for when this call is made.
        function testAPI() {
@@ -70,8 +66,4 @@ $(document).ready(function (){
              'Thanks for logging in, ' + response.name + '!';
          });
        }
-}})
-
-
-
-
+})
