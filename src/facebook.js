49a5822612de0fd609f3fb0e024e207968cda3d4
ljreader <lawrencereader@outlook.com>
2017-11-10 12:13:06 -0700

    Alpha2

--- src/facebook.js
+++ src/facebook.js
@@ -41,9 +41,3 @@ function fb_login(){
         scope: 'publish_stream,email'
     });
 }
-(function() {
-    var e = document.createElement('script');
-    e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
-    e.async = true;
-    document.getElementById('fb-root').appendChild(e);
-}());
