53cf168030240a3781a5876577c4004c1314f1b7
ljreader <lawrencereader@outlook.com>
2017-11-10 13:19:25 -0700

    hotel

--- src/facebook.js
+++ src/facebook.js
@@ -1,4 +1,12 @@
 //--Facebook script for "Custom" buttons--//
+$(document).ready(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=1517689598316225';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+//-- end of custom script--//
 
 //--Facebook link--//
        window.fbAsyncInit = function() {
