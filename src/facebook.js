957ae4aff87c6dc6afd1c4b94b600c9451363b54
ljreader <lawrencereader@outlook.com>
2017-11-09 18:42:37 -0700

    Echo

--- src/facebook.js
+++ src/facebook.js
@@ -20,22 +20,26 @@ $(document).ready(function (){
        }
        
 
-       window.fbAsyncInit = function() {
-       FB.init({
-         appId      : '1994680544133095',
+        window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1994680544133095',
          cookie     : true,  // enable cookies to allow the server to access
                              // the session
          xfbml      : true,  // parse social plugins on this page
          version    : 'v2.8' // use graph api version 2.8
-       });
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
 
-       (function(d, s, id) {
-         var js, fjs = d.getElementsByTagName(s)[0];
-         if (d.getElementById(id)) return;
-         js = d.createElement(s); js.id = id;
-         js.src = "//connect.facebook.net/en_US/sdk.js";
-         fjs.parentNode.insertBefore(js, fjs);
-       }(document, 'script', 'facebook-jssdk'));
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
 
 
        // Now that we've initialized the JavaScript SDK, we call
@@ -67,3 +71,7 @@ $(document).ready(function (){
          });
        }
 }})
+
+
+
+
